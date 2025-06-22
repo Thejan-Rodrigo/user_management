06804e5132c986417b8e8fa3eb2b5753281aa738
Thejan_Rodrigo <thejanPandula2003@gmail.com>
2025-06-22 17:00:24 +0530

    Create a Table for show the user data.

--- Frontend/user_management/src/components/cards/SendApi.jsx
+++ Frontend/user_management/src/components/cards/SendApi.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
+import "../styles/SendApi.css";
 
 function SendApi() {
     const [users, setUsers] = useState([]);
@@ -13,30 +14,123 @@ function SendApi() {
         //setShowResult("ture");
     }
 
-  return (
-    <div>
-        <h1>
-            Get All Users By Id
-        </h1>
-        <button onClick={handleClick}>
-            Send
-        </button>
+    return (
+        <div>
+            {/* <table id="customers">
+                <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                </tr>
+                <tr>
+                    <td>Berglunds snabbköp</td>
+                    <td>Christina Berglund</td>
+                    <td>Sweden</td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                </tr>
+                <tr>
+                    <td>Ernst Handel</td>
+                    <td>Roland Mendel</td>
+                    <td>Austria</td>
+                </tr>
+                <tr>
+                    <td>Island Trading</td>
+                    <td>Helen Bennett</td>
+                    <td>UK</td>
+                </tr>
+                <tr>
+                    <td>Königlich Essen</td>
+                    <td>Philip Cramer</td>
+                    <td>Germany</td>
+                </tr>
+                <tr>
+                    <td>Laughing Bacchus Winecellars</td>
+                    <td>Yoshi Tannamuri</td>
+                    <td>Canada</td>
+                </tr>
+                <tr>
+                    <td>Magazzini Alimentari Riuniti</td>
+                    <td>Giovanni Rovelli</td>
+                    <td>Italy</td>
+                </tr>
+                <tr>
+                    <td>North/South</td>
+                    <td>Simon Crowther</td>
+                    <td>UK</td>
+                </tr>
+                <tr>
+                    <td>Paris spécialités</td>
+                    <td>Marie Bertrand</td>
+                    <td>France</td>
+                </tr>
+            </table> */}
 
-        {users.length > 0 && (
+
+
+            <div class="card">
+                <div class="tools">
+                    <div class="circle">
+                        <span class="red box"></span>
+                    </div>
+                    <div class="circle">
+                        <span class="yellow box"></span>
+                    </div>
+                    <div class="circle">
+                        <span class="green box"></span>
+                    </div>
+                </div>
+                <div class="card__content">
+                    <p class="get_text">
+                        Get All Users By Id
+                    </p>
+
+                    <button onClick={handleClick}>
+                        Send
+                    </button>
+                </div>
+            </div>
+
+            {users.length > 0 && (
                 <div>
-                {users.map(item => (
-                        <li>
-                            ID: {item.ID}<br></br>
-                            First Name: {item.FristName}<br></br>
-                            Last Name: {item.LastName}<br></br>
-                            Age: {item.Age}
-                        </li> // Adjust based on your data structure
-                    ))}
-                </div>  
+                    <table id='customers'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Frist Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                        </tr>
+
+                        {users.map(item => (
+
+                            <tr>
+                                <td>{item.ID}</td>
+                                <td>{item.FristName}</td>
+                                <td>{item.LastName}</td>
+                                <td>{item.Age}</td>
+                            </tr>
+                            // <li>
+                            //     ID: {item.ID}<br></br>
+                            //     First Name: {item.FristName}<br></br>
+                            //     Last Name: {item.LastName}<br></br>
+                            //     Age: {item.Age}
+                            // </li> // Adjust based on your data structure
+                        ))}
+
+                    </table>
+                </div>
             )}
-        
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default SendApi
